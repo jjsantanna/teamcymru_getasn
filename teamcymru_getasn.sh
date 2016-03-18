ae81288eab76d4bdf2f538dad8f439f23e50faed
Jair Santanna <jairsantanna@gmail.com>
2016-03-18 14:36:35 +0100

    updating the script

--- teamcymru_getasn.sh
+++ teamcymru_getasn.sh
@@ -2,8 +2,8 @@
 
 file=$1
 
-echo -e 'begin\ncountrycode\nprefix\nnoheader' > output.txt
-cat $file >> output.txt
-echo 'end' >> output.txt
-netcat whois.cymru.com 43 < output.txt| tail -n +2 |awk -F '|' '{gsub(/ /, "", $0); gsub(",", "-", $5); print $1";"$2";"$3";"$4";"$5} ' 
-
+echo -e 'begin\ncountrycode\nprefix\nnoheader' > input.txt
+cat $file >> input.txt
+echo 'end' >> input.txt
+netcat whois.cymru.com 43 < input.txt| tail -n +2 |awk -F '|' '{gsub(/ /, "", $0); gsub(",", "-", $5); print $1";"$2";"$3";"$4";"$5} ' 
+rm input.txt
